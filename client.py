a8bd2677f515eaba570dbff69083dad5e0909e40
Jason Rowley <jasonrowley@verizon.net>
2018-03-12 21:53:14 -0400

    keep client on

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 from socket import *
 
-serverName = '127.0.0.1'
-serverPort = 61012
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-operation = input('Input operator and two integers:')
-clientSocket.send(str(operation).encode())
-result = clientSocket.recv(1024)
-print('Result From Server: ' + result.decode().split(' ')[1] + " Status: " + result.decode().split(' ')[0])
+while True:
+    serverName = '127.0.0.1'
+    serverPort = 61012
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    operation = input('Input operator and two integers:')
+    clientSocket.send(str(operation).encode())
+    result = clientSocket.recv(1024)
+    print('Result From Server: ' + result.decode().split(' ')[1] + " Status: " + result.decode().split(' ')[0])
 
-clientSocket.close()
